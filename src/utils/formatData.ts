0737163acedc9573ddfbc6f4c3ebf43ec2392fd6
Siddhu31124 <kunurusiddhartha@gmail.com>
2024-10-25 20:23:52 +0530

    #

--- src/utils/formatData.ts
+++ src/utils/formatData.ts
@@ -1,36 +1,21 @@
-interface Data {
-    id: number;
-    transaction_name: string;
-    type: string;
-    category: string;
-    amount: number;
-    date: string;
-    user_id: number;    
+import { TransactionData } from "../Types/CommonTypes";
+
+interface data {
+    transaction_name:string,
+    type: data.type,
+    category: data.category,
+    amount: data.amount,
+    date: data.date,
+    userId: data.user_id,
+
 }
-
-export default function formatData(data:Data[]){
-    let updatedData=data.map((eachItems)=>({
-        name:eachItems.transaction_name,
-        id:eachItems.id,
-        type:eachItems.type,
-        category:eachItems.category,
-        amount:eachItems.amount,
-        date:eachItems.date,
-        userId:eachItems.user_id
-    }))
-    return updatedData
-}
-
-export function formateObjData(data:Data){
-
-    return {
-        name:data.transaction_name,
-        id:data.id,
-        type:data.type,
-        category:data.category,
-        amount:data.amount,
-        date:data.date,
-        userId:data.user_id
-    }
-
+export default function formatData(data: ) {
+  return {
+    name: data.transaction_name,
+    type: data.type,
+    category: data.category,
+    amount: data.amount,
+    date: data.date,
+    userId: data.user_id,
+  };
 }
